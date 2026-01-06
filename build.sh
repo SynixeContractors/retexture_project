b2287dace38c5cda037d2fe43aefc3df883afd6f
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-06 01:08:46 +0100

    buildscript

---
+++ build.sh
@@ -0,0 +1,10 @@
+for i in $(find . -name "*.png" -not -path "*camos*" )
+do
+    cd "/data2/SteamLibrary/steamapps/common/Arma 3 Tools/ImageToPAA"
+    echo "wine ImageToPAA.exe input/$i output/${i%.png}.paa"
+    wine ImageToPAA.exe input/$i output/${i%.png}.paa
+done
+
+cd "/home/thisconnect/arma/Factions"
+hemtt dev
+
