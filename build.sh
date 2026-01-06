81e7f6325359a235b02bd914d697cb783f4d8fd9
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-06 02:52:11 +0100

    incremental build

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 for i in $(find . -name "*.png" -not -path "*camos*" )
 do
+    if [[ input/$i -ot output/${i%.png}.paa ]]; then
+       echo "$i is older than ${i%.png}.paa skipping"
+       continue
+    fi
     cd "/data2/SteamLibrary/steamapps/common/Arma 3 Tools/ImageToPAA"
     echo "wine ImageToPAA.exe input/$i output/${i%.png}.paa"
     wine ImageToPAA.exe input/$i output/${i%.png}.paa
